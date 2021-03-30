20a26a3024d2b3a464777679f9a289ad7ef1fda8
melheb.younes <melheb.younes@gmail.com>
2021-03-30 21:33:33 +0200

    update help page

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports = {
     run: message => message.channel.send(new Discord.MessageEmbed()
         .setTitle("Hello, I am here to help you")
-        .setDescription("**!link** => pour avoir le lien d'emargement \n\n **!drive** => pour le lien du drive ")
+        .setDescription("**!link** => pour avoir le lien d'emargement \n\n **!drive** => pour le lien du drive \n\n __***Réserver pour l'admin***__ \n\n **!kick @member** => pour exclure un utilisateur \n\n **!ban @member** => pour bannir un utilisateur")
         .setColor('RED')),
     name: 'help'
 }
